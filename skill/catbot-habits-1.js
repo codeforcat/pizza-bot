365a80d72bed0bc8bb954ba4f93c1d0bf6d81f68
Kazuki Imamura <kaizumaki@gmail.com>
2019-01-11 00:00:14 +0900

    スキルの不具合を修正

--- skill/catbot-habits-1.js
+++ skill/catbot-habits-1.js
@@ -24,7 +24,7 @@ module.exports = class CatbotHabits1 {
           template: {
             type: "buttons",
             thumbnailImageUrl: "https://www.dropbox.com/s/g7ith35zqjzhw6p/27_thumbnail.png?dl=1",
-            imageBackgroundColor: "#acf7fe",
+            imageBackgroundColor: "#fefae1",
             text: "どれが気になるかな？",
             actions: new_actions_array.length > 0 ? new_actions_array : actions_array
           }
@@ -52,8 +52,60 @@ module.exports = class CatbotHabits1 {
               text: `${value}が気になるんだね。`
             });
           }
+          let message;
+          if (context.confirmed.select_habits === "ゴロゴロ"){
+            message = "ゴロゴロという鳴き声には、いろんな意味があるよ。\n大体は、ご機嫌なときのゴロゴロなんだけど、「ご飯ちょうだい」などの要求がらみのものやカラダの具合が悪いときにもゴロゴロするので、気をつけて見ておいてね。"
+          } else if (context.confirmed.select_habits === "ふみふみ"){
+            message = "ふみふみは、赤ちゃん時代の思い出。\nネコは母乳を飲むとき、より母乳が出やすくなるように前脚で母親のおっぱいをマッサージするようにしながら飲みます。その時の心地よさや安心感を思い出すのか、大人になってからも、毛布や布団などのやわらかくて気持ちのいいものに触れると、ふみふみしたくなるみたいですね。"
+          } else if (context.confirmed.select_habits === "スリスリ"){
+            message = "縄張りの安心を得るためのマーキング。\n自分の縄張りにあるものや人に自分のにおいをつけるマーキング行動だよ。自分のにおいがついていないと落ち着かないんだね、きっと。"
+          } else if (context.confirmed.select_habits === "カカカッ"){
+            message = "ネコが窓に向かって、「カカカッ」といった鳴き声を出してることを聞いたことがありませんか？これは、獲物に対して、ネコが興奮しているときの反応で、クラッキングと言います。ちなみに、ライオンやトラは、同じネコ科の動物ですが、クラッキングはしません。"
+          }
+          await bot.queue({
+            type: "text",
+            text: message
+          });
+          if (new_habit_elements.length < 1){
+            await bot.apply_parameter("other", "もういいよ");
+          }
+        }
+      },
+      other: {
+        message_to_confirm: {
+          type: "template",
+          altText: "ほかのも気になる？",
+          template: {
+            type: "confirm",
+            text: "ほかのも気になる？",
+            actions: [
+              {
+                "type": "message",
+                "label": "知りたい",
+                "text": "ほかの習性を知りたい"
+              },
+              {
+                "type": "message",
+                "label": "もういいよ",
+                "text": "もういいよ"
+              }
+            ]
+          }
         },
-        // sub_skill: ["catbot-habits-gorogoro", "catbot-habits-fumifumi", "catbot-habits-surisuri", "catbot-habits-kakaka"]
+        parser: async (value, bot, event, context) => {
+          new_actions_array = [];
+          if (new_habit_elements.length > 0) {
+            new_habit_elements.forEach((value) => {
+              new_actions_array.push({type: "message", label: value, text: value});
+            });
+          }
+          if (["もういいよ"].includes(value)){
+            new_habit_elements = [];
+            confirmed_habits_array = [];
+            return value;
+          }
+          throw new Error();
+        }
       }
     };
 
@@ -61,52 +113,12 @@ module.exports = class CatbotHabits1 {
   }
 
   async finish(bot, event, context){
-    let message;
-    if (context.confirmed.select_habits === "ゴロゴロ"){
-      message = "ゴロゴロという鳴き声には、いろんな意味があるよ。\n大体は、ご機嫌なときのゴロゴロなんだけど、「ご飯ちょうだい」などの要求がらみのものやカラダの具合が悪いときにもゴロゴロするので、気をつけて見ておいてね。"
-    } else if (context.confirmed.select_habits === "ふみふみ"){
-      message = "ふみふみは、赤ちゃん時代の思い出。\nネコは母乳を飲むとき、より母乳が出やすくなるように前脚で母親のおっぱいをマッサージするようにしながら飲みます。その時の心地よさや安心感を思い出すのか、大人になってからも、毛布や布団などのやわらかくて気持ちのいいものに触れると、ふみふみしたくなるみたいですね。"
-    } else if (context.confirmed.select_habits === "スリスリ"){
-      message = "縄張りの安心を得るためのマーキング。\n自分の縄張りにあるものや人に自分のにおいをつけるマーキング行動だよ。自分のにおいがついていないと落ち着かないんだね、きっと。"
-    } else if (context.confirmed.select_habits === "カカカッ"){
-      message = "ネコが窓に向かって、「カカカッ」といった鳴き声を出してることを聞いたことがありませんか？これは、獲物に対して、ネコが興奮しているときの反応で、クラッキングと言います。ちなみに、ライオンやトラは、同じネコ科の動物ですが、クラッキングはしません。"
-    }
-    await bot.queue({
+    await bot.reply({
       type: "text",
-      text: message
+      text: "ほかに気になることがあったら話しかけてね！"
     });
-
-    if (new_habit_elements.length > 0){
-      new_actions_array = [];
-      new_habit_elements.forEach((value) => {
-        new_actions_array.push({type:"message",label:value,text:value});
-      });
-      await bot.reply({
-        type: "template",
-        altText: "ほかのも気になる？",
-        template: {
-          type: "confirm",
-          text: "ほかのも気になる？",
-          actions: [
-            {
-              "type": "message",
-              "label": "知りたい",
-              "text": "ほかの習性を知りたい"
-            },
-            {
-              "type": "message",
-              "label": "もういいよ",
-              "text": "もういいよ"
-            }
-          ]
-        }
-      });
-    } else {
-      await bot.reply({
-        type: "text",
-        text: "ほかに気になることがあったら話しかけてね！"
-      });
-      confirmed_habits_array = [];
-    }
+    new_actions_array = [];
+    new_habit_elements = [];
+    confirmed_habits_array = [];
   }
 };
